9cea72a1e610e9b8ebcdf627ab19970d18a2137b
Daniel-17 <frimudaniel17@gmail.com>
2022-06-05 15:20:03 +0200

    Find the position!

---
+++ find-the-position.js
@@ -0,0 +1,32 @@
+function position(letter) {
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  return `Position of alphabet: ${alphabet.indexOf(letter.toUpperCase()) + 1}`;
+}
