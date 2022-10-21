1b9968ad5ed9749f26210c1391e026f630e469d2
Daniel-17 <frimudaniel17@gmail.com>
2022-10-21 16:34:19 +0200

    stringify numbers

---
+++ stringify-numbers.js
@@ -0,0 +1,82 @@
+/*
+let obj = {
+    num: 1,
+    test: [],
+    data: {
+        val: 4,
+        info: {
+            isRight: true,
+            random: 66
+        }
+    }
+}
+
+stringifyNumbers(obj)
+
+{
+    num: "1",
+    test: [],
+    data: {
+        val: "4",
+        info: {
+            isRight: true,
+            random: "66"
+        }
+    }
+}
+*/
+
+let obj1 = {
+  num: 1,
+  test: [],
+  data: {
+    val: 4,
+    info: {
+      isRight: true,
+      random: 66,
+    },
+  },
+};
+
+// MY SOLUTION
+function stringifyNumbers(obj) {
+  for (let item of Object.entries(obj)) {
+    if (typeof item[1] === "number") {
+      obj[item[0]] = item[1].toString();
+    } else {
+      stringifyNumbers(item[1]);
+    }
+  }
+  return obj;
+}
+
+// SOLUTION OF OTHERS
+function stringifyNumbers(obj) {
+  var newObj = {};
+  for (var key in obj) {
+    if (typeof obj[key] === "number") {
+      newObj[key] = obj[key].toString();
+    } else if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+      newObj[key] = stringifyNumbers(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+}
+
+stringifyNumbers(obj1);
+
+/* 
+{
+    num: "1",
+    test: [],
+    data: {
+        val: "4",
+        info: {
+            isRight: true,
+            random: "66"
+        }
+    }
+}
+*/
