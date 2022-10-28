d265cacb942790205a2b12890e33dcb657c28407
Daniel-17 <frimudaniel17@gmail.com>
2022-10-28 16:16:42 +0200

    ddl shift method

---
+++ ddl-shift-method.js
@@ -0,0 +1,29 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    const currentHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = currentHead.next;
+      this.head.prev = null;
+      currentHead.next = null;
+    }
+    this.length--;
+    return currentHead;
+  }
+}
