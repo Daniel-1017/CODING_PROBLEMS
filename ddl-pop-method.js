1cd77cd81e287914098437e1698e09c1810f15ea
Daniel-17 <frimudaniel17@gmail.com>
2022-10-28 16:56:00 +0200

    ddl pop method

---
+++ ddl-pop-method.js
@@ -0,0 +1,29 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  pop() {
+    if (!this.head) return null;
+    const currentTail = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = currentTail.prev;
+      this.tail.next = null;
+      currentTail.prev = null;
+    }
+    this.length--;
+    return removedNode;
+  }
+}
