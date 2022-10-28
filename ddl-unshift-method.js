1695a1ac653dbc8d78cd8d8dc060869ea081a7b1
Daniel-17 <frimudaniel17@gmail.com>
2022-10-28 16:30:15 +0200

    ddl unshift method

---
+++ ddl-unshift-method.js
@@ -0,0 +1,28 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  unshift(val) {
+    const node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.head.prev = node;
+      node.next = this.head;
+      this.head = node;
+    }
+    this.length++;
+    return this;
+  }
+}
