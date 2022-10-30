a38efde331d897a14482f89987023793b6f42754
Daniel-17 <frimudaniel17@gmail.com>
2022-10-30 16:45:34 +0100

    fizz buzz

---
+++ fizz-buzz.js
@@ -0,0 +1,24 @@
+/* 
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+*/
+
+const fizzBuzz = function (n) {
+  const fizzArr = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      fizzArr.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      fizzArr.push("Fizz");
+    } else if (i % 5 === 0) {
+      fizzArr.push("Buzz");
+    } else {
+      fizzArr.push(i.toString());
+    }
+  }
+  return fizzArr;
+};
