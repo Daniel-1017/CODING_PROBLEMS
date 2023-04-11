f4fd41db7fd7bf17ad4df94610bf3038f5415869
Daniel-17 <frimudaniel17@gmail.com>
2023-04-11 19:36:16 +0200

    Alternate case

---
+++ alternate-case.js
@@ -0,0 +1,9 @@
+/* 
+Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+*/
+
+function alternateCase(s) {
+  return [...s].map(l =>
+    l === l.toUpperCase() ? l.toLowerCase() : l.toUpperCase()
+  ).join``;
+}
