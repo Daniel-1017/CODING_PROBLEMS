76bd46a9869195ad21b0373fde6d66d3fb5dd351
Daniel-17 <frimudaniel17@gmail.com>
2023-04-22 10:41:55 +0200

    Make the Deadfish Swim

--- move-10.js
+++ move-10.js
@@ -6,8 +6,15 @@ If it goes past 'z', start again at 'a'.
 Input will be a string with length > 0.
 */
 
+// MY SOLUTION
 function moveTen(s) {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
   return [...s].map(l => alphabet[(l.charCodeAt() - 87) % alphabet.length])
     .join``;
 }
+
+// SOLUTION OF OTHERS
+const moveTen = s =>
+  s.replace(/./g, val =>
+    String.fromCharCode(97 + ((val.charCodeAt() - 87) % 26))
+  );
