252de598c280882eb19f0acf1484849b641cdba0
Daniel-17 <frimudaniel17@gmail.com>
2023-05-18 17:27:31 +0200

    Convert Hash To An Array

---
+++ convert-hash-to-an-array.js
@@ -0,0 +1,19 @@
+/* 
+Convert a hash into an array. Nothing more, Nothing less.
+
+{name: 'Jeremy', age: 24, role: 'Software Engineer'}
+should be converted into
+
+[["age", 24], ["name", "Jeremy"], ["role", "Software Engineer"]]
+Note: The output array should be sorted alphabetically by key name.
+*/
+
+// MY SOLUTION
+function convertHashToArray(hash) {
+  return Object.keys(hash)
+    .sort()
+    .map(key => [key, hash[key]]);
+}
+
+// SOLUTION OF OTHERS
+const convertHashToArray = o => Object.entries(o).sort();
