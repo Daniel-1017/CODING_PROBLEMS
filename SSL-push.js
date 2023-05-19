d5110b1dfcf6ee74b6c14a61c8c2a79e6acf2345
Daniel-17 <frimudaniel17@gmail.com>
2023-05-19 22:38:45 +0200

    SSL push

---
+++ SSL-push.js
@@ -0,0 +1,25 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(data) {
+    const node = new Node(data);
+    if (!this.head) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    return this;
+  }
+}
