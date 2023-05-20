3ec2540510b9b29566a937258fc5c52141ab2672
Daniel-17 <frimudaniel17@gmail.com>
2023-05-20 11:14:19 +0200

    Return substring instance count

---
+++ substring-instance-count.js
@@ -0,0 +1,6 @@
+function solution(fullText, searchText) {
+  const regex = new RegExp(`${searchText}`, "g");
+  return fullText.match(regex).length;
+}
+
+console.log(solution("aaabbbcccc", "bbb"));
