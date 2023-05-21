c9025ac47b13f6decbeb83af1e8480cf3c413a18
Daniel-17 <frimudaniel17@gmail.com>
2023-05-21 19:26:45 +0200

    Number Zoo Patrol

--- number-zoo-patrol.js
+++ number-zoo-patrol.js
@@ -5,6 +5,3 @@ function findNumber(array) {
   const sum = array.reduce((acc, curr) => acc + curr, 0);
   return total - sum === 0 ? max + 1 : total - sum;
 }
-
-// console.log(findNumber([1, 2, 3]));
-console.log(findNumber([13, 11, 10, 3, 2, 1, 4, 5, 6, 9, 7, 8]));
