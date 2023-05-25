4f164da5e326f8cdb029894b72358b1c31958475
Daniel-17 <frimudaniel17@gmail.com>
2023-05-25 15:58:03 +0200

    Build a pile of Cubes

---
+++ build-a-pile-of-cubes.js
@@ -0,0 +1,27 @@
+/* 
+Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of n3, the cube above will have volume of (n-1)3 and so on until the top which will have a volume of 1 ** 3.
+You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
+
+Examples:
+findNb(1071225) --> 45
+
+findNb(91716553919377) --> -1
+*/
+
+// MY SOLUTION
+function findNb(m) {
+  let v = 1,
+    i = 2;
+  while (v < m) {
+    v += i ** 3;
+    i++;
+  }
+  return v === m ? i - 1 : -1;
+}
+
+// SOLUTION OF OTHERS
+function findNb(m) {
+  let n = 0;
+  while (m > 0) m -= (++n) ** 3;
+  return m ? -1 : n;
+}
