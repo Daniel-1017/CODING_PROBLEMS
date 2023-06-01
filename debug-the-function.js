6659dee8a0acffdb502bce2687861faef7854bcd
Daniel-17 <frimudaniel17@gmail.com>
2023-06-01 12:00:17 +0200

    Debug the functions

---
+++ debug-the-function.js
@@ -0,0 +1,15 @@
+/* 
+Should be easy, begin by looking at the code. Debug the code and the functions should work.
+
+There are three functions: Multiplication (x) Addition (+) and Reverse (!esreveR)
+*/
+
+function multi(arr) {
+  return arr.reduce((acc, val) => acc * val, 1);
+}
+function add(arr) {
+  return arr.reduce((acc, val) => acc + val, 0);
+}
+function reverse(str) {
+  return [...str].reverse().join``;
+}
