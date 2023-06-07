0bca998b5ae16579a9cc47f94f3c88bc52770db9
Daniel-17 <frimudaniel17@gmail.com>
2023-06-07 23:48:08 +0200

    Reverse every other word in the string

---
+++ reverse-every-other-word-in-the-string.js
@@ -0,0 +1,73 @@
+/* 
+Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+*/
+
+function reverse(str) {
+  const abc = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  if (!str.length) return "";
+  return str.split(" ").map((word, idx) => {
+    if (idx % 2 === 1) {
+      const punctuation = abc.includes(word[word.length - 1]);
+      if (!punctuation) {
+        return (
+          word[word.length - 1] + word.slice(0, -1).split("").reverse().join``
+        );
+      }
+      return word.split("").reverse().join``;
+    }
+    return word;
+  }).join` `.trim();
+}
