1fb93069ddee545fefeffdb9ac72d5085612dd1c
Daniel-17 <frimudaniel17@gmail.com>
2023-06-08 23:37:34 +0200

    Pizza Payments

---
+++ pizza-payments.js
@@ -0,0 +1,14 @@
+/* 
+Kate and Michael want to buy a pizza and share it. Depending on the price of the pizza, they are going to divide the costs:
+
+If the pizza is less than €5,- Michael invites Kate, so Michael pays the full price.
+Otherwise Kate will contribute 1/3 of the price, but no more than €10 (she's broke :-) and Michael pays the rest.
+How much is Michael going to pay? Calculate the amount with two decimals, if necessary.
+*/
+
+function michaelPays(costs) {
+  if (costs < 5) return costs;
+  const katePay = +(costs / 3).toFixed(2);
+  if (katePay > 10) return costs - 10;
+  return costs - katePay;
+}
