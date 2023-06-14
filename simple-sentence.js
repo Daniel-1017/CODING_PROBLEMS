f0e8cdf9532f97be509d0ba2b1a5cdb6c9588eae
Daniel-17 <frimudaniel17@gmail.com>
2023-06-14 13:29:25 +0200

    Simple Sentences

---
+++ simple-sentence.js
@@ -0,0 +1,28 @@
+/* 
+Implement a function, so it will produce a sentence out of the given parts.
+
+Array of parts could contain:
+
+words;
+commas in the middle;
+multiple periods at the end.
+Sentence making rules:
+
+there must always be a space between words;
+there must not be a space between a comma and word on the left;
+there must always be one and only one period at the end of a sentence.
+Example:
+
+makeSentence(['hello', ',', 'my', 'dear']) // returns 'hello, my dear.'
+*/
+
+function makeSentence(parts) {
+  let sentence = "";
+  for (let i = 0; i < parts.length; i++) {
+    if (parts[i] === ",") sentence += ", ";
+    else if (i === parts.length - 1) sentence += parts[i] + ".";
+    else if (parts[i + 1] === ",") sentence += parts[i];
+    else sentence += parts[i] + " ";
+  }
+  return sentence.split("").filter(char => char !== ".").join``.trim() + ".";
+}
