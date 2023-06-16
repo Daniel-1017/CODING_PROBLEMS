8593f1baf7ac06bb80e09f15e8693b3acce57efc
Daniel-17 <frimudaniel17@gmail.com>
2023-06-16 11:09:01 +0200

    Santa's Naughty List

--- funny-dots.js
+++ funny-dots.js
@@ -1,3 +1,18 @@
+/* 
+Funny Dots
+You will get two integers n (width) and m (height) and your task is to draw the following pattern. Each line is seperated with a newline (\n)
+
+Both integers are equal or greater than 1; no need to check for invalid parameters.
+
+Examples
+
+                                            +---+---+---+
+             +---+                          | o | o | o |
+dot(1, 1) => | o |            dot(3, 2) =>  +---+---+---+            
+             +---+                          | o | o | o |
+                                            +---+---+---+
+*/
+
 function dot(w, h) {
   const hWall = "---+";
   const vWall = " o |";
@@ -8,5 +23,3 @@ function dot(w, h) {
   }
   return string.slice(0, -1);
 }
-
-dot(4, 4);
