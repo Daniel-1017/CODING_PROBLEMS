047d45bb24b99d663c1dc0fd949d9c33e94977fc
Daniel-17 <frimudaniel17@gmail.com>
2023-06-18 17:51:44 +0200

    Count number of zeros from 1 to N

---
+++ count-n-of-zeroes-from-1-to-n.js
@@ -0,0 +1,24 @@
+/* 
+Create an algorithm to count the number of zeros that appear between 1 and N.
+
+Examples:
+
+There are 2 zeros from 1 to 20: 10, 20
+
+There are 11 zeros from 1 to 100: 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+
+countZeros(10); // returns 1
+countZeros(100); // returns 11
+countZeros(200); // returns 31
+*/
+
+function countZeros(n) {
+  let count = 0;
+  for (let i = 0; i <= n; i++) {
+    count += i
+      .toString()
+      .split("")
+      .filter(n => n == 0).length;
+  }
+  return count - 1;
+}
