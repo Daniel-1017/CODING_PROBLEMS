f8a3e320d857d274864d1fcd5c4a4b121e0e736a
Daniel-1017 <frimudaniel17@gmail.com>
2023-07-09 11:19:26 +0300

    Get initials from person name

---
+++ get-initials-from-person-name.js
@@ -0,0 +1,9 @@
+/* 
+Write function toInitials returs initials for a given person name. E.g: "Bill Gates" -> "B. G."
+
+Note: initials should be separated with a space.
+*/
+
+function toInitials(name) {
+  return name.split(" ").map((n) => `${n[0]}.`).join` `;
+}
