f9986bf061c6260c49fdff7cb0fd15cdba9f7046
Daniel-1017 <frimudaniel17@gmail.com>
2023-07-27 02:19:19 +0300

    letters only, please!

---
+++ letters-only.js
@@ -0,0 +1,27 @@
+/* 
+Let's assume we need "clean" strings. Clean means a string should only contain letters a-z, A-Z and spaces. We assume that there are no double spaces or line breaks.
+
+Write a function that takes a string and returns a string without the unnecessary characters.
+
+Examples
+removeChars('.tree1')    ==> 'tree'
+
+removeChars("that's a pie$ce o_f p#ie!")  ==> 'thats a piece of pie'
+
+removeChars('john.dope@dopington.com')    ==> 'johndopedopingtoncom'
+
+removeChars('my_list = ["a","b","c"]')    ==> 'mylist  abc'
+
+removeChars('1 + 1 = 2')    ==> '    ' (string with 4 spaces)
+
+removeChars("0123456789(.)+,|[]{}=@/~;^$'<>?-!*&:#%_")  ==> '' (empty string)
+*/
+
+function removeChars(s) {
+  let result = "";
+  for (let i of s) {
+    if (i === " ") result += " ";
+    else if (/[a-z]/gi.test(i)) result += i;
+  }
+  return result;
+}
