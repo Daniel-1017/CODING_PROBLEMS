8b3f17a00b56b5cd93dca8129cac718c716f0ddf
Daniel-1017 <frimudaniel17@gmail.com>
2023-08-13 09:44:03 +0300

    Product Of Maximums Of Array

---
+++ product-of-maximums-in-array.js
@@ -0,0 +1,21 @@
+/* 
+Task
+Given an array/list [] of integers , Find the product of the k maximal numbers.
+
+Notes
+Array/list size is at least 3 .
+
+Array/list's numbers Will be mixture of positives , negatives and zeros
+
+Repetition of numbers in the array/list could occur.
+
+Input >> Output Examples
+maxProduct ({4, 3, 5}, 2) ==>  return (20)
+*/
+
+function maxProduct(numbers, size) {
+  return numbers
+    .sort((a, b) => b - a)
+    .slice(0, size)
+    .reduce((acc, n) => acc * n, 1);
+}
