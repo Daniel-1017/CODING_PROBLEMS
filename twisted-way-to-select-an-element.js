9bae0681bd574bb9b97173bb25c187e7ec9a9afe
Frimu Daniel <frimudaniel17@gmail.com>
2023-12-03 17:43:30 +0100

    A twisted way to select an element from an array

---
+++ twisted-way-to-select-an-element.js
@@ -0,0 +1,13 @@
+/* 
+You have two arrays: arrSearch and arrTake. arrSearch has to be sorted in reverse alphabetical order. Now, from arrSearch extract the third element, and from that element select the third letter. If the letter you selected matches the first letter of one or more elements of arrTake, return the first element that starts with the respective letter. If there is no element to match in the second array then return 'Nothing here'.
+*/
+
+function select(arrSearch, arrTake) {
+  arrSearch.sort((a, b) => b.localeCompare(a));
+  const thirdLetterFromThirdElement = arrSearch[2][2];
+
+  for (const word of arrTake)
+    if (word[0] === thirdLetterFromThirdElement) return word;
+
+  return "Nothing here";
+}
