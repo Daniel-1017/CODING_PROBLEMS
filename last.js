6199d8d4b834b37d3489a6b635521ff152e1e203
Frimu Daniel <frimudaniel17@gmail.com>
2023-12-30 11:23:41 +0100

    Last

---
+++ last.js
@@ -0,0 +1,21 @@
+/* 
+Find the last element of the given argument(s).
+
+Examples
+last([1, 2, 3, 4] ) // =>  4
+last("xyz")         // => "z"
+last(1, 2, 3, 4)    // =>  4
+In javascript and CoffeeScript a list will be an array, a string or the list of arguments.
+*/
+
+// MY SOLUTION
+function last(arg) {
+  if ((typeof arg === "string" || Array.isArray(arg)) && arguments.length === 1) return arg[arg.length - 1]
+  return Array.from(arguments)[arguments.length - 1]
+}
+
+// SOLUTION OF OTHERS
+function last(list){
+  var last = arguments[arguments.length - 1];
+  return last[last.length - 1] || last;
+}
