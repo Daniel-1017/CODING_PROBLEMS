b4c7a9b5979ade70ff90640db92fa594cd94c125
Frimu Daniel <frimudaniel17@gmail.com>
2024-02-01 20:43:03 +0100

    Maxed Out

---
+++ maxed-out.js
@@ -0,0 +1,12 @@
+/* 
+For this task you will be given an array of numbers, each number in turn has to be cubed and once all numbers have been cubed all of them have to be added to get a final sum.
+
+If the final sum is less than or equal to the maximum value allowed for an Integer return the sum otherwise return "You've pushed me to the max!".
+
+You can assume that you will receive a valid array of numbers.
+*/
+
+function maxedOut(arr) {
+  const sum = arr.reduce((acc, n) => acc + Math.pow(n, 3), 0)
+  return sum > Number.MAX_SAFE_INTEGER  ? "You've pushed me to the max!" : sum
+}
