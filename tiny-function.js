49cbbfed8cac1c0da53aee63f294c684a9025fa0
Frimu Daniel <frimudaniel17@gmail.com>
2024-02-28 20:20:21 +0100

    "Tiny" function

---
+++ tiny-function.js
@@ -0,0 +1,13 @@
+/* 
+Our universe is so huge, possibly even infinite and it makes us feel so tiny, isn't that so?
+
+Task
+Write a tiny function f that outputs 'tiny' without using the letters t, i, n, y anywhere in your code
+
+Code Limit
+Your code is limited to less than 34 characters
+
+Buffer and unicode escapes \ are banned to avoid cheesy solutions :)
+*/
+
+f=_=>($=1/0+"")[6]+$[3]+$[1]+$[7]
