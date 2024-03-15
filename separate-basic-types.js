6e4b11c0bcd6cfc5ed4e31e60e297b69e0d3aea3
Frimu Daniel <frimudaniel17@gmail.com>
2024-03-15 20:59:52 +0100

    Separate basic types

---
+++ separate-basic-types.js
@@ -0,0 +1,22 @@
+/* 
+Given: a sequence of different type of values (number, string, boolean). You should return an object with a separate properties for each of types presented in input. Each property should contain an array of corresponding values.
+
+keep order of values like in input array
+if type is not presented in input, no corresponding property are expected
+So, for this input:
+
+['a', 1, 2, false, 'b']
+expected output is:
+
+{
+  number: [1, 2],
+  string: ['a', 'b'],
+  boolean: [false]
+}
+*/
+
+const separateTypes = input => {
+    const output = {};
+    for (const type of input) output[typeof type] = (output[typeof type] || []).concat(type);
+    return output;
+}
