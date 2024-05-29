cf347dab6c267c944c6c1ac01826af4dbdf718cd
Frimu Daniel <frimudaniel17@gmail.com>
2024-05-29 22:51:44 +0200

    Array Info

---
+++ array-info.js
@@ -0,0 +1,50 @@
+/* 
+Brief
+Sometimes we need information about the list/arrays we're dealing with. You'll have to write such a function in this kata. Your function must provide the following informations:
+
+Length of the array
+Number of integer items in the array
+Number of float items in the array
+Number of string character items in the array
+Number of whitespace items in the array
+The informations will be supplied in arrays that are items of another array. Like below:
+
+Output array = [[array length],[no of integer items],[no of float items],[no of string chars items],[no of whitespace items]]
+
+Added Difficulty
+If any item count in the array is zero, you'll have to replace it with a None/nil/null value (according to the language). And of course, if the array is empty then return 'Nothing in the array!. For the sake of simplicity, let's just suppose that there are no nested structures.
+Output ====== If you're head is spinning (just kidding!) then these examples will help you out-
+
+   
+array_info([1,2,3.33,4,5.01,'bass','kick',' '])--------->[[8],[3],[2],[2],[1]]    
+array_info([0.001,2,' '])------------------------------>[[3],[1],[1],[None],[1]]   
+array_info([])----------------------------------------->'Nothing in the array!'
+array_info([' '])-------------------------------------->[[1],[None],[None],[None],[1]]
+  
+Remarks
+The input will always be arrays/lists. So no need to check the inputs.
+Hint ==== See the tags!!!
+Now let's get going !
+*/
+
+const arrayInfo = arr => {
+  let ints = 0,
+    floats = 0,
+    strs = 0,
+    blanks = 0;
+  for (const val of arr) {
+    if (val === " ") blanks++;
+    else if (typeof val === "string") strs++;
+    else if (typeof val === "number" && parseInt(val) === val) ints++;
+    else if (typeof val === "number" && parseInt(val) !== val) floats++;
+  }
+  return arr.length
+    ? [
+        [arr.length],
+        [ints || null],
+        [floats || null],
+        [strs || null],
+        [blanks || null],
+      ]
+    : "Nothing in the array!";
+};
